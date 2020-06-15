6af659814e8c68e10bf2b0271e0699eabff42c99
Tomas Skogberg <tomas.skogberg@gmail.com>
2020-06-15 15:43:38 +0200

    Revert "Update rails -> 6.0.3.1"
    This most likely broke the build record persist.
    
    This reverts commit 176008962f95d2ea4e4431c145818709344c6249.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
+  force_ssl if: :production?
+
   private
+
+  def production?
+    Rails.env.production?
+  end
 
   def locals(action = nil, hash)
     render action: action, locals: hash

64c5a08f3d9496902b4b132a8998a43da2e89241
Tomas Skogberg <tomas.skogberg@gmail.com>
2020-12-11 08:25:56 +0100

    Update Rails -> 6.1.0
    https://weblog.rubyonrails.org/2020/12/9/Rails-6-1-0-release/
    https://guides.rubyonrails.org/upgrading_ruby_on_rails.html#upgrading-from-rails-6-0-to-rails-6-1

--- db/schema.rb
+++ db/schema.rb
@@ -1,44 +1,44 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140216162823) do
+ActiveRecord::Schema.define(version: 2014_02_16_162823) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
 
-  create_table "builds", force: :cascade do |t|
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.string   "status",      limit: 255, null: false
-    t.string   "name",        limit: 255, null: false
-    t.integer  "revision_id"
-    t.string   "status_url",  limit: 255
+  create_table "builds", id: :serial, force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "status", limit: 255, null: false
+    t.string "name", limit: 255, null: false
+    t.integer "revision_id"
+    t.string "status_url", limit: 255
   end
 
-  create_table "projects", force: :cascade do |t|
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
-    t.string   "name",       limit: 255,             null: false
-    t.string   "repository", limit: 255
-    t.text     "mappings"
-    t.integer  "position",               default: 0
+  create_table "projects", id: :serial, force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name", limit: 255, null: false
+    t.string "repository", limit: 255
+    t.text "mappings"
+    t.integer "position", default: 0
   end
 
-  create_table "revisions", force: :cascade do |t|
-    t.string   "name",       limit: 255, null: false
-    t.integer  "project_id",             null: false
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+  create_table "revisions", id: :serial, force: :cascade do |t|
+    t.string "name", limit: 255, null: false
+    t.integer "project_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

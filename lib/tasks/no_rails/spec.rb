24099d80a3d939aae55ca9cfe72321e563d15af6
Olle Jonsson <olle.jonsson@auctionet.com>
2021-11-19 09:43:33 +0100

    fix: Make it loadable even when no RSpec gem
    If in an environment where there is no RSpec, this rescue LoadError
    allows the file to present itself anyway.

--- lib/tasks/no_rails/spec.rb
+++ lib/tasks/no_rails/spec.rb
@@ -1,9 +1,12 @@
-require "rspec/core/rake_task"
-RSpec::Core::RakeTask.new(:spec)
+begin
+  require "rspec/core/rake_task"
+  RSpec::Core::RakeTask.new(:spec)
 
-RSpec::Core::RakeTask.new("spec:unit") do |t|
-  t.rspec_opts = "-r#{File.expand_path("unit/spec_helper.rb")}"
-  t.pattern = "unit/**/*_spec.rb"
+  RSpec::Core::RakeTask.new("spec:unit") do |t|
+    t.rspec_opts = "-r#{File.expand_path("unit/spec_helper.rb")}"
+    t.pattern = "unit/**/*_spec.rb"
+  end
+
+  task default: %w[ spec:unit spec ]
+rescue LoadError
 end
-
-task default: %w[ spec:unit spec ]

3960ef6760c21b102e68ddeacdbdac240dcf1fbd
Olle Jonsson <olle.jonsson@auctionet.com>
2021-11-19 09:56:47 +0100

    docs: Disambiguate Rake task descriptions [ci skip]
    Now, the output is:
    
        $ rake -T spec
        rake spec       # Run RSpec tests in spec/
        rake spec:unit  # Run RSpec tests in unit/

--- lib/tasks/no_rails/spec.rb
+++ lib/tasks/no_rails/spec.rb
@@ -1,7 +1,9 @@
 begin
   require "rspec/core/rake_task"
+  desc "Run RSpec tests in spec/"
   RSpec::Core::RakeTask.new(:spec)
 
+  desc "Run RSpec tests in unit/"
   RSpec::Core::RakeTask.new("spec:unit") do |t|
     t.rspec_opts = "-r#{File.expand_path("unit/spec_helper.rb")}"
     t.pattern = "unit/**/*_spec.rb"

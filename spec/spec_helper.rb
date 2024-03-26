8cde4ea2461af0d6089707c2ab9ccffa0def2c0e
Olle Jonsson <olle.jonsson@auctionet.com>
2024-03-26 14:53:20 +0100

    Cleanup: use Pathname#glob

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ require File.expand_path("../config/environment", __dir__)
 require "rspec/rails"
 require "factory_bot_rails"
 
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Rails.root.glob("spec/support/**/*.rb") { |f| require f }
 
 RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false

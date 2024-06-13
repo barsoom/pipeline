df43f1211350b357fb15e8d46738c8e1e90feedf
Peter Wall <peter@auctionet.com>
2024-06-13 16:23:43 +0200

    fix nested list
    Co-authored-by: Joakim Kolsjö <13256+joakimk@users.noreply.github.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
-  config.action_cable.allowed_request_origins = [ production_hosts ]
+  config.action_cable.allowed_request_origins = production_hosts
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   unless ENV["DISABLE_SSL"]

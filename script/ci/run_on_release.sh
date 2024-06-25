ddb2a14346527c901aa8bc950cf2fa572e6e56c4
Peter Wall <peter@auctionet.com>
2024-06-25 11:45:19 +0200

    make releases take a long time, to test concurrent releases

--- script/ci/run_on_release.sh
+++ script/ci/run_on_release.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 echo "Running migrations"
 bundle exec bin/rails db:migrate
+
+# to test concurrent releases in the stack
+if [ "$HONEYBADGER_ENV" == "stack" ]; then
+  for i in {1..120}; do
+    echo "Waiting for 10 minutes, $i/120"
+    sleep 5
+  done
+fi

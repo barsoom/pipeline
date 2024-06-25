9cce8e69b9e07178e5f95b4f91997d2e0d6fba93
Peter Wall <peter@auctionet.com>
2024-06-25 12:11:25 +0200

    Revert "make releases take a long time, to test concurrent releases"
    This reverts commit ddb2a14346527c901aa8bc950cf2fa572e6e56c4.

--- script/ci/run_on_release.sh
+++ script/ci/run_on_release.sh
@@ -1,14 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
 echo "Running migrations"
 bundle exec bin/rails db:migrate
-
-# to test concurrent releases in the stack
-if [ "$HONEYBADGER_ENV" == "stack" ]; then
-  for i in {1..120}; do
-    echo "Waiting for 10 minutes, $i/120"
-    sleep 5
-  done
-fi

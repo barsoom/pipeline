71c007a4ecb8799bec5731fd6b43618283b2e95a
Olle Jonsson <olle.jonsson@auctionet.com>
2024-07-24 17:11:54 +0200

    Puma: configure raise_exception_on_sigterm false

--- config/puma.rb
+++ config/puma.rb
@@ -2,6 +2,8 @@ workers Integer(ENV["WEB_CONCURRENCY"] || 2)
 threads_count = Integer(ENV["RAILS_MAX_THREADS"] || 5)
 threads threads_count, threads_count
 
+raise_exception_on_sigterm false # On receiving SIGTERM, just exit gracefully.
+
 preload_app!
 
 port        ENV["PORT"]     || 3000

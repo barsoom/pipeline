8ee4afdde29b4e2fef00114128ccedfd378ee05a
Olle Jonsson <olle.jonsson@auctionet.com>
2024-08-28 13:14:57 +0200

    cleanup: Prefer Rails.env.local?

--- config/application.rb
+++ config/application.rb
@@ -39,8 +39,7 @@ module Pipeline
     # Enable the asset pipeline
     config.assets.enabled = true
 
-    dev_or_test = Rails.env.test? || Rails.env.development?
-    default_key = dev_or_test ? "test" : nil
+    default_key = Rails.env.local? ? "test" : nil
     config.secret_key_base = ENV["SECRET_KEY_BASE"] || default_key
 
     config.middleware.insert_after ActionDispatch::Session::CookieStore, JwtAuthentication, ignore: [
@@ -54,7 +53,7 @@ module Pipeline
       puts "You must set SECRET_KEY_BASE. Generate one with 'rake secret'."
 
       # Don't break in prod, breaks heroku asset precompilation.
-      exit 1 if dev_or_test
+      exit 1 if Rails.env.local?
     end
   end
 end

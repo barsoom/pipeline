9d890aa30a8e44bb10f9bdc90c070bc4a45b85b7
Olle Jonsson <olle.jonsson@auctionet.com>
2025-07-18 09:49:33 +0200

    Redis: Configure Sentinel
    Ref AHA-737

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,13 @@
-port = ENV["DEVBOX"] ? `service_port redis`.strip : 6379
-uri = URI.parse(ENV["REDISCLOUD_URL"] || ENV["REDIS_URL"] || "redis://localhost:#{port}")
-$redis = Redis.new(host: uri.host, port: uri.port, password: uri.password, db: Rails.env.test? ? 1 : 0)
+$redis =
+  if ENV["USING_SENTINEL_URL"]
+    uri = URI.parse(ENV["SENTINEL_URL"])
+    name = uri.path.delete_prefix("/")
+    sentinels = Resolv.getaddresses(uri.host).map { { host: it, port: uri.port } }
+    sentinel_username = uri.user
+    sentinel_password = uri.password
+    Redis.new(name:, sentinels:, sentinel_username:, sentinel_password:)
+  else
+    port = ENV["DEVBOX"] ? `service_port redis`.strip : 6379
+    uri = URI.parse(ENV["REDISCLOUD_URL"] || ENV["REDIS_URL"] || "redis://localhost:#{port}")
+    Redis.new(host: uri.host, port: uri.port, password: uri.password, db: Rails.env.test? ? 1 : 0)
+  end
